4e64ab39973bb602a5c6abbc4b137d8617d23485
sahmadnakumbe-8 <59398617+sahmadnakumbe-8@users.noreply.github.com>
2022-03-13 01:31:44 -0600

    js3

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var chosenCategoryShortName = chooseRandomCategory(Categories).short_name;
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -126,7 +126,6 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
       insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
       },
       false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
